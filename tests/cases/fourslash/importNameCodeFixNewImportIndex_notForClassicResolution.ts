1b4d504564a4371fc700022ca7b05459deb055a5
Andy Hanson <anhans@microsoft.com>
2017-11-15 13:01:56 -0800

    More tests

--- tests/cases/fourslash/importNameCodeFixNewImportIndex_notForClassicResolution.ts
+++ tests/cases/fourslash/importNameCodeFixNewImportIndex_notForClassicResolution.ts
@@ -5,14 +5,28 @@
 // @Filename: /a/index.ts
 ////export const foo = 0;
 
+// @Filename: /node_modules/x/index.d.ts
+////export const bar = 0;
+
 // @Filename: /b.ts
-////[|/**/foo;|]
+////[|foo;|]
+
+// @Filename: /c.ts
+////bar;
 
 goTo.file("/a/index.ts");
-goTo.file("/b.ts");
 
+goTo.file("/b.ts");
 verify.importFixAtPosition([
-`import { foo } from "./a/index.ts";
+`import { foo } from "./a/index";
 
 foo;`
 ]);
+
+// TODO: GH#20050 verify.not.codeFixAvailable();
+goTo.file("/c.ts");
+verify.importFixAtPosition([
+`import { bar } from "./node_modules/x/index";
+
+bar;
+`]);
